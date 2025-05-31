77e261f4a667afa29fc3d94d0bd8eab0412cd509
sygns13 <cristian_7_70@hotmail.com>
2025-05-31 13:20:11 -0500

    avance modulos

--- src/app/pages/chatbot/chatbot.component.ts
+++ src/app/pages/chatbot/chatbot.component.ts
@@ -1,11 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, NgModule, ElementRef, ViewChild, AfterViewChecked } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PaginatorModule } from 'primeng/paginator';
+import { FluidModule } from 'primeng/fluid';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
+import { TextareaModule } from 'primeng/textarea';
+import { SelectModule  } from 'primeng/select';
+import { TableModule } from 'primeng/table';
+
+import { MessageService, ToastMessageOptions } from 'primeng/api';
+import { MessageModule } from 'primeng/message';
+import { ToastModule } from 'primeng/toast';
+import { MenuItem } from 'primeng/api';
+import { PanelMenuModule } from 'primeng/panelmenu';
+import { ConsultagptService } from 'src/app/services/consultagpt.service';
+import { IRequest, GptResponse, GptHistoryItem, HistoryResponse } from 'src/app/interfaces/consultagpt';
+import { ExpedientesService } from 'src/app/services/expedientes.service';
+import { Sede, Instancia, Especialidad, Expediente, BusquedaExpediente } from 'src/app/interfaces/expedientes';
+import { TipodocumentoService } from 'src/app/services/tipodocumento.service';
+import { DocumentoService } from 'src/app/services/documento.service';
+import { marked } from 'marked';
+import { SoloNumerosEnterosDirective } from '../directives/solo-numeros-enteros.directive';
+import { Dropdown } from 'primeng/dropdown';
+import { TooltipModule } from 'primeng/tooltip';
+import { DialogModule } from 'primeng/dialog';
+import { TipoDocumento } from 'src/app/interfaces/tipodocumento';
+import { Documento } from 'src/app/interfaces/documento';
+
+const environment = (window as any).__env as any;
 
 @Component({
   selector: 'app-chatbot',
-  imports: [],
+  imports: [CommonModule, SelectModule , TableModule, InputTextModule, FluidModule, ButtonModule, FormsModule, TextareaModule, MessageModule, ToastModule, PanelMenuModule, PaginatorModule, SoloNumerosEnterosDirective, TooltipModule, DialogModule],
+  providers: [MessageService],
   templateUrl: './chatbot.component.html',
   styleUrl: './chatbot.component.scss'
 })
 export class ChatbotComponent {
+
+  private env = environment;
+    
+      displayFiltros: boolean = false;
+    
+      constructor(
+        private service: MessageService,
+        private documentoService: DocumentoService,
+        private tipodocumentoService: TipodocumentoService,
+        private expedientesService: ExpedientesService) {
+    
+        console.log('Environment from Microfront:');
+        console.log(this.env);
+      }
+  
 
 }
